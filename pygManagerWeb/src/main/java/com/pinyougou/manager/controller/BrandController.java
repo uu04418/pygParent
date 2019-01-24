19329e1a1df12d5669b544ea27ca3c6400e35316
tom_glb <goodMoring_glb@163.com>
2019-01-24 22:17:11 +0800

    带条件分页查询品牌

--- pygManagerWeb/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pygManagerWeb/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -79,6 +79,14 @@ public class BrandController {
 		}
 	}
 	
+	
+	@ResponseBody
+	@RequestMapping("/search")
+	public PageResult search (@RequestBody TbBrand brand ,
+			int page ,int size	) {
+		return brandService.findPage(brand, page, size);
+	}
+	
 
 	
 }
