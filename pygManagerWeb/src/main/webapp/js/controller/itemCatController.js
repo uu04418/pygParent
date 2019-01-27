f9386087de55e93b5732918116a18bed4b34d5ef
uu04418 <uu04418@163.com>
2019-01-27 16:44:52 +0800

    解决导航 问题

--- pygManagerWeb/src/main/webapp/js/controller/itemCatController.js
+++ pygManagerWeb/src/main/webapp/js/controller/itemCatController.js
@@ -85,6 +85,27 @@ app.controller('itemCatController' ,function($scope,$controller   ,itemCatServic
 				$scope.list=response;
 			}			
 		);
-	}    
+	}   
+	
+	$scope.grade = 1;
+	$scope.setGrade = function (value) {
+		$scope.grade = value ;
+	};
+	
+	$scope.selectList = function (p_entity) {
+		if ($scope.grade == 1) {
+			$scope.entity_1 = null;
+			$scope.entity_2 = null;
+		}
+		if ($scope.grade == 2) {
+			$scope.entity_1 = p_entity;
+			$scope.entity_2 = null;
+		}
+		if ($scope.grade == 3) {
+			$scope.entity_2 = p_entity;
+		}
+		$scope.findByParentId(p_entity.id);
+		
+	}
     
 });	
