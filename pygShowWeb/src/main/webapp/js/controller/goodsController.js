dc4bf98002133865609a03d67e574882f4ab2cac
uu04418 <uu04418@163.com>
2019-01-28 22:45:40 +0800

    商品上传图片

--- pygShowWeb/src/main/webapp/js/controller/goodsController.js
+++ pygShowWeb/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('goodsController' ,function($scope,$controller   ,goodsService){	
+app.controller('goodsController' ,function($scope,$controller ,goodsService ,uploadService){	
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -93,5 +93,27 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService){
 			}			
 		);
 	}
+	
+	$scope.uploadFile = function () {
+		uploadService.uploadFile().success(
+			function (response) {
+				if (response.success) {
+					$scope.image_entity.url = response.message;
+				}else {
+					alert(response.message);
+				}
+			}
+		);
+	}
+	$scope.entity={ goodsDesc:{itemImages:[]}  };
+	$scope.add_image_entity = function () {
+		$scope.entity.goodsDesc.itemImages.push($scope.image_entity);
+	}
+	
+	//移除图片
+	$scope.remove_image_entity=function(index){
+		$scope.entity.goodsDesc.itemImages.splice(index,1);
+	}
+    
     
 });	
