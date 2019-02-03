13ae03523c444048c958592258b6432dd4b4b14c
uu04418 <uu04418@163.com>
2019-02-03 19:16:08 +0800

    搭建门户前台

--- pygManagerWeb/src/main/webapp/js/controller/contentController.js
+++ pygManagerWeb/src/main/webapp/js/controller/contentController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('contentController' ,function($scope,$controller   ,contentService){	
+app.controller('contentController' ,function($scope,$controller   ,contentService,uploadService,contentCategoryService){	
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -76,5 +76,27 @@ app.controller('contentController' ,function($scope,$controller   ,contentServic
 			}			
 		);
 	}
+	
+	$scope.uploadFile = function () {
+		uploadService.uploadFile().success(
+			function (response) {
+				if (response.success) {
+					$scope.entity.pic = response.message;
+				}else {
+					alert(response.message);
+				}
+			}
+		);
+	}
+	
+	$scope.contentCategoryList = function () {
+		contentCategoryService.findAll().success(
+			function (response) {
+				$scope.contentCategoryList = response;
+			}
+		);
+	}
+	
+	$scope.status = ['无效','有效'];
     
 });	
